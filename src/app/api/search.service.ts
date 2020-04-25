c73f705fce5b1472edf57a76bb5fc2a2755bdeb2
he23inw3 <ma7k5px@gmail.com>
2020-04-26 00:55:08 +0900

    string-format導入

--- src/app/api/search.service.ts
+++ src/app/api/search.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
+import { Book } from '../model/book';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { String } from 'typescript-string-operations';
+import { BookDetails } from '../model/book-details';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +13,21 @@ import { HttpClient } from '@angular/common/http';
 export class SearchService {
 
   constructor(private http: HttpClient) { }
+
+  /**
+   * 書籍検索
+   * @param page ペース数
+   * @param q 検索文字
+   */
+  searchBookInfo(page: number, q: string): Observable<Book> {
+    return this.http.get<Book>(String.Format(environment.searchBookInfoUrl, page, q));
+  }
+
+  /**
+   * 書籍詳細検索
+   * @param id ID
+   */
+  searchDetailsBook(id: string): Observable<BookDetails> {
+    return this.http.get<BookDetails>(String.Format(environment.searchBookDetailsInfoUrl, id));
+  }
 }
